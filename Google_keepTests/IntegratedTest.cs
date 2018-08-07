e72d9dbe45c30928ca293df974d586ee7935f9d4
Nikita karanpuria <nikita.karanpuria8@gmail.com>
2018-08-07 16:03:39 +0530

    dummy

---
+++ Google_keepTests/IntegratedTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using NSuperTest;
+
+namespace Google_keepTests
+{
+    public class IntegratedTest
+    {
+        static Server server;
+        public static void Init(TestContext ctx)
+        {
+            server = new Server();
+        }
+
+        // [Fact]
+        public IntegratedTest()
+        {
+            //static Server<Startup> server;
+            server = new Server("https://localhost:44367");
+        }
+        [Fact]
+        public void Gettest()
+        {
+            server.Get("api/Notes1").Expect(200).End();
+        }
+    }
+
+    public class TestContext
+    {
+    }
+}
+
